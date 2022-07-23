e27b5870b2c278d53bfdd2f2445868e84c59c71e
Gustavo Coutinho <gupoco@outlook.com>
2022-07-23 14:07:43 -0300

    Change secret key for Django and store it in a file which is in .gitignore

--- gov_system/gov_system/settings.py
+++ gov_system/gov_system/settings.py
@@ -10,18 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+
 import os
+from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7(_qkhofu=h(w*xzzgl8%_1#p(tdvi64(+x%vmav!zoz2j$kc)'
-
+# Old and insecure secret key is described below
+# SECRET_KEY = 'django-insecure-7(_qkhofu=h(w*xzzgl8%_1#p(tdvi64(+x%vmav!zoz2j$kc)'
+# Secret key from .env referenced below
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
